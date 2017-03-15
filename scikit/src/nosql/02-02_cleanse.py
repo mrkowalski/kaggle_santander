9186264468e7e09c3c6d3181f7f1c8644748d5a9
Ubuntu <ubuntu@ip-172-31-21-179.eu-central-1.compute.internal>
2017-03-15 22:10:17 +0000

    per-indicator classifier

--- scikit/src/nosql/02-02_cleanse.py
+++ scikit/src/nosql/02-02_cleanse.py
@@ -16,7 +16,7 @@ def add_activations(df):
         df["act_" + ind] = res.fillna(0)
     return df
 
-chunks = 1
+chunks = 5
 df = pd.DataFrame()
 for n in range(1, chunks+1):
     log.info('Loading dataframe...#{}'.format(n))
@@ -36,8 +36,21 @@ df = df[df['fecha_dato'].isin([pd.Timestamp('2015-06'), pd.Timestamp('2016-03'),
 log.info("After: {}".format(df.shape))
 #df.drop(['ncodpers', 'fecha_dato'], inplace=True, axis=1)
 df.drop(['ncodpers'], inplace=True, axis=1)
+df['fecha_dato'] = df.apply(lambda r: r['fecha_dato'].month, axis=1)
 
 activation_columns=["act_" + i for i in commons.indicators]
 
 for ind in commons.indicators:
     log.info("Building model for {}".format(ind))
+    X = df.drop(activation_columns, axis = 1)
+    Y = df["act_" + ind]
+#    log.info("X: {}, Y: {}".format(list(X.columns.values), Y.name))
+    clf = xgb.XGBClassifier(objective = 'binary:logistic', nthread=8, silent=1, max_depth=6)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state=42)
+    clf = clf.fit(X_train, Y_train)
+    Y_pred = clf.predict(X_test)
+    log.info("Feature importances: {}".format(sorted(zip(list(X), clf.feature_importances_), key=lambda x: x[1])))
+    log.info("Accuracy: {:.2%}, precision: {:.2%}, recall: {:.2%}".format(
+        accuracy_score(Y_test, Y_pred),
+        precision_score(Y_test, Y_pred),
+        recall_score(Y_test, Y_pred)))
