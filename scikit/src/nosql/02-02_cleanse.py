1055782c77bca1ec5a0a5f9f1250533c7295c7c1
mrkowalski <marcin.kowalski.pl@googlemail.com>
2017-03-15 14:14:04 +0100

    Update 02-02_cleanse.py

--- scikit/src/nosql/02-02_cleanse.py
+++ scikit/src/nosql/02-02_cleanse.py
@@ -8,3 +8,11 @@ from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
 
+def add_activations(df):
+    for ind in commons.indicators:
+        log.info("Adding activations for {}".format(ind))
+        ind_prev = ind + "_1"
+        res = df[ind].sub(df[ind_prev])
+        res[res < 0] = 0
+        df["act_" + ind] = res.fillna(0)
+return df
