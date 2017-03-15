a16a84b80429ee379b6704bde8a464c81a92d1d1
mrkowalski <marcin.kowalski.pl@googlemail.com>
2017-03-15 14:11:24 +0100

    Update 02-02_cleanse.py

--- scikit/src/nosql/02-02_cleanse.py
+++ scikit/src/nosql/02-02_cleanse.py
@@ -1,1 +1,10 @@
 
+import commons, sys, os
+import logging as log
+import pandas as pd
+import xgboost as xgb
+import numpy as np
+from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
+
