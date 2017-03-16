7c7976f8f4c9459edd3c8cd99971e561cd07307d
mrkowalski <marcin.kowalski.pl@googlemail.com>
2017-03-16 21:54:13 +0100

    Update 03_train_predict.py

--- scikit/src/nosql/03_train_predict.py
+++ scikit/src/nosql/03_train_predict.py
@@ -1,3 +1,25 @@
-log.info('Loading clean dataframe...')
-df = pd.read_pickle(commons.FILE_DF_CLEAN)
+import commons
+import pandas as pd
+import logging as log
 
+def load_models(indicators):
+    print("Loading models... ", end='', flush=True)
+    models = {i: commons.read_model(i) for i in indicators}
+    print("Done.")
+    return models
+
+chunks = 1
+df = pd.DataFrame()
+for n in range(1, chunks+1):
+    log.info('Loading dataframe...#{}'.format(n))
+    df = df.append(pd.read_hdf(commons.FILE_DF + "." + str(n), key='santander'))
+
+df = df[df['is_test_data']]
+df.drop(['is_test_data'], inplace=True, axis=1)
+df.drop(commons.indicators_ignored, inplace=True, axis=1)
+df.drop(commons.indicators ,inplace=True,axis=1)
+
+all_models = load_models(commons.indicators)
+submission = {}
+
+log.info("{}".format(list(df)))
