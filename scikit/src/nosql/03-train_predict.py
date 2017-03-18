8d9b62ce9f410cae072924bd1486d10c1efe80a9
Ubuntu <ubuntu@ip-172-31-21-179.eu-central-1.compute.internal>
2017-03-18 19:18:21 +0000

    most likely predictions first

--- scikit/src/nosql/03-train_predict.py
+++ scikit/src/nosql/03-train_predict.py
@@ -9,7 +9,6 @@ def load_models(indicators):
     return models
 
 df = pd.read_hdf('dataframe.hdf5.test', key='santander')
-print(df.head())
 df.drop(commons.indicators, inplace=True, axis=1)
 
 log.info("Number of test cases: {}".format(df.shape[0]))
@@ -25,12 +24,10 @@ for indicator in commons.indicators:
     log.info("Processing: {}".format(indicator))
     model = all_models[indicator]
     for decision in zip(customers, model.predict_proba(df)):
-        log.info("{}".format(decision))
-        sys.exit()
         if decision[0]  not in submission: submission[decision[0]] = list()
-        if decision[1]: submission[decision[0]].append(indicator)
+        if decision[1][1] >= decision[1][0]: submission[decision[0]].append((indicator, decision[1][1]))
 
 with open('submission.csv', 'w') as submission_file:
     print("ncodpers,added_products", file=submission_file)
     for k, v in submission.items():
-        print("{},{}".format(k, " ".join(set(v))), file=submission_file)
+        print("{},{}".format(k, " ".join([i[0] for i in sorted(v, reverse=True, key=lambda e: e[1])])), file=submission_file)
