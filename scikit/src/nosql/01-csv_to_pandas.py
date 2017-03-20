06b2618cd1199937be7e0f3e42e9713a51fc5350
Ubuntu <ubuntu@ip-172-31-21-179.eu-central-1.compute.internal>
2017-03-20 00:32:39 +0000

    silly ifx

--- scikit/src/nosql/01-csv_to_pandas.py
+++ scikit/src/nosql/01-csv_to_pandas.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def load(f, is_test_data):
     log.info('Loading {}...'.format(f))
-    df = pd.read_csv(f, sep=',', nrows=50000)
+    df = pd.read_csv(f, sep=',')
     df.drop(['tipodom', 'cod_prov', 'conyuemp', 'fecha_alta', 'ult_fec_cli_1t'], inplace=True, axis=1)
     df.insert(0, 'is_test_data', is_test_data)
     return df
